372005c854ff89494abc8e49d860920c4351c53f
Henry Liu <codicate@gmail.com>
2022-10-14 20:38:05 -0400

    firebase config finished

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
+    projectId: 'dandyhack22',
+    appId: '1:412426358157:web:2df0864e0211a9411e01c4',
+    storageBucket: 'dandyhack22.appspot.com',
+    locationId: 'us-central',
     apiKey: 'AIzaSyCjYH6GCWm4zsxIzbC-6AWBNwzUp9e1kGE',
     authDomain: 'dandyhack22.firebaseapp.com',
-    projectId: 'dandyhack22',
-    storageBucket: 'dandyhack22.appspot.com',
     messagingSenderId: '412426358157',
-    appId: '1:412426358157:web:2df0864e0211a9411e01c4',
   },
 };
 
