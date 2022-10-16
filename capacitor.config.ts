14401bdbc08149934456250fe66cabed97da263d
Henry Liu <codicate@gmail.com>
2022-10-16 09:23:42 -0400

    renamed app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'Dandyhack22',
+  appName: 'Nearvents',
   webDir: 'www',
   bundledWebRuntime: false,
   cordova: {
@@ -13,9 +13,9 @@ const config: CapacitorConfig = {
       FadeSplashScreenDuration: '300',
       SplashShowOnlyFirstTime: 'false',
       SplashScreen: 'screen',
-      SplashScreenDelay: '3000'
-    }
-  }
+      SplashScreenDelay: '3000',
+    },
+  },
 };
 
 export default config;
