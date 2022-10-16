8bb9c95a2933c86b88839be5e966f76343b00616
Henry Liu <codicate@gmail.com>
2022-10-16 07:26:00 -0400

    small data bug fix

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 Social platform using event-based system to connect people together.
 
 Created by Siddharth Narsipur, Henry Liu and Suchith Hedge at the University of Rochester for Dandyhacks '22
-
-- leadership page css (Sid)
-
-- eventpage (Sid)
-
-- point system
-
-- map page styling? dark mode?
--- src/app/eventpage/eventpage.page.ts
+++ src/app/eventpage/eventpage.page.ts
@@ -41,7 +41,7 @@ export class EventpagePage implements OnInit {
     this.dataService.getID.subscribe(async (message) => {
       const user = await this.authService.getCurrentUser();
       if (message) {
-        if (message === user.id || user.events.includes(message)) {
+        if (message === user.id || user.events?.includes(message)) {
           this.blur = false;
         }
 
