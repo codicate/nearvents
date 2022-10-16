5202d4fa4de8e2fd1b6841e5e300a456c8c1ce85
Henry Liu <codicate@gmail.com>
2022-10-16 06:12:49 -0400

    brrrrooo leaftlet darkmode

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -2,9 +2,8 @@ import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 import { EventService } from 'services/event.service';
 import { CLIENT_RENEG_LIMIT } from 'tls';
-import { DataService } from 'services/data.service'
+import { DataService } from 'services/data.service';
 import { Router } from '@angular/router';
-
 
 const iconRetinaUrl = 'assets/marker-icon-2x.png';
 const iconUrl = 'assets/marker-icon.png';
@@ -17,7 +16,7 @@ const iconDefault = L.icon({
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   tooltipAnchor: [16, -28],
-  shadowSize: [41, 41]
+  shadowSize: [41, 41],
 });
 L.Marker.prototype.options.icon = iconDefault;
 
@@ -26,14 +25,24 @@ L.Marker.prototype.options.icon = iconDefault;
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
 })
-
 export class MapPage implements AfterViewInit {
+  events = [];
   private map;
-  events = []
 
-  private onPopupClick(e){
-    console.log(e)
-    this.router.navigateByUrl('tabs/tabs/eventpage/' + e,{
+  constructor(
+    private router: Router,
+    private eventService: EventService,
+    private dataService: DataService
+  ) {}
+
+  async ngAfterViewInit() {
+    this.events = await this.eventService.getAllEvents();
+    this.initMap();
+  }
+
+  private onPopupClick(e) {
+    console.log(e);
+    this.router.navigateByUrl('tabs/tabs/eventpage/' + e, {
       replaceUrl: true,
     });
     this.dataService.getCurrentID(e);
@@ -41,40 +50,31 @@ export class MapPage implements AfterViewInit {
   private initMap(): void {
     this.map = L.map('map', {
       renderer: L.canvas(),
-      center: [43.128806,-77.630220],
-      zoom: 16
+      center: [43.128806, -77.63022],
+      zoom: 16,
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
+    const tiles = L.tileLayer(
+      'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
+      {
+        maxZoom: 20,
+        minZoom: 5,
+      }
+    );
 
     tiles.addTo(this.map);
-    for (let e of this.events) {
+    for (const e of this.events) {
       const popup = L.DomUtil.create('div', 'infoWindow');
-      popup.innerHTML = `<center> ${e.name} </center> <img src="${e.banner}"> `;
+      popup.innerHTML = `<div style="width:max-content;display:flex;flex-direction:column;justify-content:center;gap:10px;padding-top:20px;"><img width="150px" height="200px" style="object-fit:cover;" src="${e.banner}"><center style="font-size:1rem;"> ${e.name} </center></div>`;
       const marker = new L.Marker(e.location);
       marker.bindPopup(popup);
-      L.DomEvent.addListener(popup, 'click', () => this.onPopupClick(e.creatorPlayerID  ));
+      L.DomEvent.addListener(popup, 'click', () => {
+        this.onPopupClick(e.creatorPlayerID);
+      });
       marker.addTo(this.map);
     }
-    setTimeout(()=> { this.map.invalidateSize()}, 500);
-
-  }
-
-  constructor(
-    private router: Router,
-    private eventService: EventService,
-    private dataService: DataService
-  ) { }
-
-  async ngOnInit() {
-  }
-
-  async ngAfterViewInit() {
-    this.events = await this.eventService.getAllEvents();
-    this.initMap();
+    setTimeout(() => {
+      this.map.invalidateSize();
+    }, 500);
   }
 }
